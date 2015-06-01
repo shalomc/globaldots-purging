c29e3a01333cd192a2ed7be9954d1e5e0aeb264d
shalomc <shalom@globaldots.com>
2015-06-02 00:54:13 +0300

    Cleaning up Hibernia

--- Vendors/Hibernia/purge-hibernia.bat
+++ Vendors/Hibernia/purge-hibernia.bat
@@ -2,25 +2,9 @@
 @setlocal
 
 :: Sample usage: 
-:: 		purge-hibernia ZoneID  "regular-expression"
+:: 		purge-hibernia SiteID  "URL"
 :: 
 
-FOR /F "tokens=* usebackq" %%a IN (`php -f purge-hibernia.php `) DO (
-	set OAUTH=%%a
-	)
-
-set BASE_URL=https://portal.hiberniacdn.com
-:: set BASE_URL=http://echo.httpkit.com
-set ACTION=api/purges.json
-:: php -f purge-hibernia.php %* > token.txt
-curl -i -H "Authorization: Bearer %OAUTH%" -H "content-type: application/json" -d @hibernia.json -X POST "%BASE_URL%/%ACTION%" 
-:: curl -i -H "Authorization: Bearer %OAUTH%"  -d "site_id=98" -d "site_id=98" -X POST "%BASE_URL%/%ACTION%"
-
-:: logout not working
-:: curl -i -H "Authorization: Bearer %OAUTH%" -H "content-type: application/json" -d "{""bearer_token"":""%OAUTH%""}" -X POST "%BASE_URL%/%ACTION%" 
-:: curl -i -H "Authorization: Bearer %OAUTH%" -d "bearer_token=%OAUTH%" -X POST "%BASE_URL%/%ACTION%" 
-
-set ACTION=api/logout.json
-curl -i -H "Authorization: Bearer %OAUTH%" -X POST "%BASE_URL%/%ACTION%" 
+php -f purge-hibernia.php %* 
 :finish
 echo.
